786860c691cf6aaef681770ea254659b5a0a91f1
Michael Duffy <micduffy@gmail.com>
2020-05-18 15:07:12 +1000

    Bump Jackson

--- src/main/java/io/biza/lab/api/controller/BackendController.java
+++ src/main/java/io/biza/lab/api/controller/BackendController.java
@@ -2,8 +2,6 @@ package io.biza.lab.api.controller;
 
 
 import io.biza.lap.api.BankingApiController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.NativeWebRequest;
 
 public class BackendController extends BankingApiController {
