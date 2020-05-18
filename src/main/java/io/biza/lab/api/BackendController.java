39f8c2dc89a13fb5332b87568d216aea782979dd
Michael Duffy <micduffy@gmail.com>
2020-05-18 14:31:55 +1000

    API implement

--- src/main/java/io/biza/lab/api/BackendController.java
+++ src/main/java/io/biza/lab/api/BackendController.java
@@ -1,6 +1,19 @@
 package io.biza.lab.api;
 
 
+import io.biza.lap.api.AccountsApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 public class BackendController  {
+    @Autowired
+    AccountsApi accountsApi;
+    @GetMapping("/search")
+    public String search(@RequestParam(value = "q") String q) {
+//        accountsApi.getBalance()
+        return q;
+    }
+
 
 }
