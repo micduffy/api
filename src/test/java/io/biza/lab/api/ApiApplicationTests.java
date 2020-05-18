c9b9b846841cfb228d67457a845c0424dc8a1604
Michael Duffy <micduffy@gmail.com>
2020-05-18 14:08:00 +1000

    first commit

---
+++ src/test/java/io/biza/lab/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.biza.lab.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
